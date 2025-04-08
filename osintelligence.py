7d944eda02ba7ca9c44959ee63966b4991097980
XYKA! <140552462+Xyka01@users.noreply.github.com>
2025-04-08 21:36:30 +0300

    Update osintelligence.py

--- osintelligence.py
+++ osintelligence.py
@@ -1,1 +1,14 @@
-print("Hello, World!")
+import webbrowser
+
+print("")
+print("OSİNT MASTER PROFESSİONAL TOOL's")
+print("")
+print("[01] >> TEST SİTESİNİ AÇ")
+choice = input("Bir seçenek girin: ")
+
+# 1. seçenek seçildiğinde YouTube'u aç
+if choice == "1":
+    webbrowser.open("https://www.youtube.com")
+    print("YouTube açılıyor...")
+else:
+    print("Geçersiz seçenek.")

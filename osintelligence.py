593a967e5dbc4482636de0e738e6c7f633575cbd
XYKA! <140552462+Xyka01@users.noreply.github.com>
2025-04-08 22:36:55 +0300

    GÜNCELLEME YAMASI : MENÜLER EKLENİYOR.

--- osintelligence.py
+++ osintelligence.py
@@ -1,15 +1,76 @@
-import webbrowser
+#PROGRAM 08.04.2025 / 22:05 Zaman ve Tarihleri içinde XYKA! Tarafından yapılmıştır.
+#PROGRAMın tüm Yazılımı ve Özelleştirilmesi XYKA! tarafından yapılmıştır.
+#Bu kodun İzinsiz bir şekilde alınıp özelleştirilmesi durumunda versiyon düşürme işlemi uygulanır.
+print("")
+print("__  ____   ___  __    _    _ ")
+print("\ \/ /\ \ / / |/ /   / \  | |")
+print(" \  /  \ V /| ' /   / _ \ | |")
+print(" /  \   | | | . \  / ___ \|_|")
+print("/_/\_\  |_| |_|\_\/_/   \_(_)")  
+print("Profesyonel İstihbarat Araçları by - XYKA!")
+print("")
+print("[01] >> OSİNT .NET İSTİHBARATI")
+print("[02] >> ELİNT .ELW İSTİHBARATI")
+print("[03] >> SIGINT .SIG İSTİHBARATI")
+print("")
+print("[04] >> PROGRAMDAN ÇIKIŞ")
+print("[05] >> YAZILIMCI .İNF")
+print("")
+girisecenek = input("Bir seçenek girin: ")
 
-print("")
-print("OSİNT MASTER PROFESSİONAL TOOL's")
-print("")
-print("[01] >> TEST SİTESİNİ AÇ")
-print("")
-choice = input("Bir seçenek girin: ")
 
-# 1. seçenek seçildiğinde YouTube'u aç
-if choice == "1":
-    webbrowser.open("https://www.youtube.com")
-    print("YouTube açılıyor...")
+if girisecenek == "1":
+    print(".NET OSİNT / VERİYE DAYALI İSTİHBARAT SEÇENEKLERİ")
+    print("")
+    print("[01] >> USERNAME")
+    print("[02] >> EMAİL ADRESS")
+    print("[03] >> DOMAİN NAME")
+    print("[04] >> IP-MAC ADRESS")
+    print("[05] >> IMAGES-VİDEOS-DOCS")
+    print("[06] >> SOCİAL NETWORKS")
+    print("[07] >> INSTAT MESSAGES")
+    print("[08] >> PEOPLE SEARCH ENGİNES")
+    print("[09] >> DATİNG")
+    print("[10] >> TELEPHONE NUMBER")
+    print("[11] >> PUBLİC RECORDS")
+    print("[12] >> BUSİNESS RECORDS")
+    print("[13] >> TRANSPORTATİON")
+    print("[14] >> GEOLOCATİON TOOLS")
+    print("[15] >> SEARCH ENGİNE")
+    print("[16] >> FORUMS-BLOGS-IRC")
+    print("[17] >> ARCHİVES")
+    print("[18] >> LANGUAGE TRANLATİON")
+    print("[19] >> METADATA") 
+    print("[20] >> MOBİLE EMULATİON")
+    print("[21] >> TERRORİSM")
+    print("[22] >> DARK WEB")
+    print("[23] >> DİGİTAL CURRENCY")
+    print("[24] >> CLASSİFİEDS")
+    print("[25] >> ENCODİNG-DECODİNG")
+    print("[26] >> TOOLS")
+    print("[27] >> AI TOOLS")
+    print("[28] >> MALİCİOUS FİLE ")
+    print("[29] >> EXPLOİT")
+    print("[30] >> THREAT INTELLIGENCE")
+    print("[31] >> OPSEC")
+    print("[32] >> EVİDENCE CAPTURE")
+    print("[33] >> TRAİNİNG")
+    print("[34] >> XYKA!")
+
+
+
+elif girisecenek == "2":
+    print("SEÇENEK 2 - ELİNT .ELW İSTİHBARATI")
+
+elif girisecenek == "3":
+    print("SEÇENEK 3 - SIGINT .SIG İSTİHBARATI")
+
+elif girisecenek == "4":
+    print("PROGRAMDAN ÇIKIYOR...")
+    exit() 
+
+elif girisecenek == "5":
+    print("YAZILIMCI BİLGİSİ: XYKA")
+
 else:
     print("Geçersiz seçenek.")
